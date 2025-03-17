83e12f116d53012cf044fdbe93c833a8f8d509ef
Ulises <r2r2eydeitaca@gmail.com>
2025-03-17 16:45:26 +0100

    17/03/2025

--- LandingPageRetroWave/JavaScript.js
+++ LandingPageRetroWave/JavaScript.js
@@ -5,30 +5,17 @@ const wrapper = document.querySelector('.carrusel-wrapper');
 const carruselImages = document.querySelector('.carrusel-images');
 
 // Clonar las imágenes y agregarlas al final para crear el efecto de bucle infinito
-images.forEach(img => {
-    const clone = img.cloneNode(true); // Clonar cada imagen
-    carruselImages.appendChild(clone); // Agregar el clon al final
-});
+function clonacion() {
+    images.forEach(img => {
+        const clone = img.cloneNode(true); // Clonar cada imagen
+        carruselImages.appendChild(clone); // Agregar el clon al final
+    });
+}
 
 // Mover el carrusel hacia la derecha
 function moveNext() {
+    clonacion();
     currentIndex++;
-
-    // Si llegamos al final de las imágenes originales, reiniciamos el índice
-    if (currentIndex >= totalImages) {
-        currentIndex = 0;
-
-        // Mover el carrusel al principio sin transición
-        wrapper.style.transition = 'none';
-        wrapper.style.transform = `translateX(0)`;
-
-        // Forzar un reflujo para aplicar el cambio
-        wrapper.offsetHeight;
-
-        // Reactivar la transición
-        wrapper.style.transition = 'transform 0.5s ease-in-out';
-    }
-
     // Actualizar el desplazamiento del carrusel
     updateCarrusel();
 }
