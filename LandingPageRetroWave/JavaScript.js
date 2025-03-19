492188b72c26189855a8976c059c0e7d2e3f7d8f
Ulises <r2r2eydeitaca@gmail.com>
2025-03-19 10:12:59 +0100

    19/03/2025

--- LandingPageRetroWave/JavaScript.js
+++ LandingPageRetroWave/JavaScript.js
@@ -22,7 +22,7 @@ function moveNext() {
 
 // Function to Update the movement
 function updateCarrusel() {
-    const offset = -currentIndex * (images[0].clientWidth + 20); // Adjust the movement
+    const offset = -currentIndex * (images[0].clientWidth + 30); // Adjust the movement
     wrapper.style.transform = `translateX(${offset}px)`;
 }
 
@@ -35,49 +35,34 @@ setInterval(moveNext, 3000); // Slide each 3 seconds
 //********Part of filtration************
 
 $(document).ready(function() {
-    
-    const allImages = document.querySelectorAll('.GamesSection .Games img');
-    allImages.forEach(img => {
-        if (!img.classList.contains('btn')) {
-            img.style.display = 'none'; // Hide images without the class btn
-        }
-    });
-    // Save a copy of the structure
+
+    $('.GamesSection .Games img').not('.btn').hide();
+    // Guarda una copia de la estructura inicial
     const gamesSectionHTML = $('.GamesSection .Games').html();
 
-    // Function to restore all
     function restoreAll() {
         $('.GamesSection .Games').html(gamesSectionHTML); 
-        $('.back-button').remove(); // Delete "Volver Atras" button
-        $('.GamesSection h2').text('Juegos'); 
-        attachEventHandlers(); 
+        $('.GamesSection .Games img').not('.btn').hide(); // Ocultar todas las imágenes relacionadas
+        $('.back-button').remove(); // Eliminar botón "Volver Atrás"
+        $('.GamesSection h2').text('Juegos');
+        attachEventHandlers();
     }
 
-    // Function to asign again the buttons to all the images etc
     function attachEventHandlers() {
         $('.container-image').click(function() {
-            
             var containerId = $(this).closest('div').attr('id');
 
-            
-            $('.GamesSection .Games img, .GamesSection .Games .container-image').remove();
+            // Elimina todas las imágenes y contenedores de imágenes
+            $('.GamesSection .Games .container-image').hide();
 
-            //Show images that are inside same id that the btn and show all except the button
-            $('#' + containerId + ' img').not('.btn').each(function() {
-                $(this).clone().appendTo('.GamesSection .Games'); 
-            });
+            //Parte que me mata
+            $('#' + containerId + ' img').not('.btn').show();
 
-            allImages.forEach(img => {
-                if (img.classList.contains('#' + containerId + ' img')) {
-                    img.style.display = 'flex';
-                }
-            });
-
-            
+            // Cambia el título con la categoría seleccionada
             var categoryName = $(this).find('.container-text p').text();
             $('.GamesSection h2').text('Juegos - ' + categoryName);
 
-            // Add the button "Volver Atras"
+            // Agregar botón "Volver atrás"
             $('<button class="back-button">Volver atras</button>')
                 .appendTo('.GamesSection')
                 .click(function() {
@@ -86,6 +71,6 @@ $(document).ready(function() {
         });
     }
 
-    
     attachEventHandlers();
 });
+
